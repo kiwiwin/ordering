3786a5d77ff053badf89aa1b35409a12ed7c185f
kiwi <jcwen@thoughtworks.com>
2014-07-02 12:09:41 +0800

    add payment not found 404

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,5 +3,6 @@ class PaymentsController < ApplicationController
 		@user = User.find(params[:user_id].to_i)
 		@order = Order.where(id: params[:order_id].to_i).first
 		@payment = @order.payment
+		render :nothing => true, :status => :not_found unless @payment
 	end
 end
--- spec/payments/payments_controller_spec.rb
+++ spec/payments/payments_controller_spec.rb
@@ -6,19 +6,31 @@ describe PaymentsController, :type => :controller do
 	fixtures :all
 
 	describe 'GET payment of order' do
-		before {
-			get :show, {:user_id => 1, :order_id => 1, :format => :json}
-		}
+		context 'when order already pay' do
+			before {
+				get :show, {:user_id => 1, :order_id => 1, :format => :json}
+			}
 
-		it 'have http status 200' do
-			expect(response).to have_http_status(200)
+			it 'have http status 200' do
+				expect(response).to have_http_status(200)
+			end
+
+			it 'is JSON formatted' do
+				payment = JSON.parse(response.body)
+
+				expect(payment['uri']).to end_with('/users/1/orders/1/payment')
+				expect(payment['amount']).to eq("10.12")
+			end
 		end
 
-		it 'is JSON formatted' do
-			payment = JSON.parse(response.body)
-			
-			expect(payment['uri']).to end_with('/users/1/orders/1/payment')
-			expect(payment['amount']).to eq("10.12")
+		context 'when order has not pay' do
+			before {
+				get :show, {:user_id => 1, :order_id => 2, :format => :json}
+			}
+
+			it 'have http status 404' do
+				expect(response).to have_http_status(404)
+			end
 		end
 	end
 end
