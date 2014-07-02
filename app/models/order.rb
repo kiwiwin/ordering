c4d2d028ef8b5819d202c703eee15c12f38e4075
kiwi <jcwen@thoughtworks.com>
2014-07-02 10:41:35 +0800

    add order price

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  def price
+  	product.price
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Order, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	fixtures :all
+
+	describe 'price' do
+		it 'is the product price' do
+			order = orders(:apple_juice_order)
+			expect(order.price).to eq(10.12)
+		end
+	end
 end
