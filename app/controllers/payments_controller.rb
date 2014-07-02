3c2bebeaf57ebe462993f17dfed78c6fe366109a
kiwi <jcwen@thoughtworks.com>
2014-07-02 12:21:36 +0800

    extrac before action in payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,16 +1,20 @@
 class PaymentsController < ApplicationController
+	before_action :set_order
+
 	def show
-		@user = User.find(params[:user_id].to_i)
-		@order = Order.where(id: params[:order_id].to_i).first
 		@payment = @order.payment
 		render :nothing => true, :status => :not_found unless @payment
 	end
 
 	def create
-		@user = User.find(params[:user_id].to_i)
-		@order = Order.where(id: params[:order_id].to_i).first
 		@payment = @order.build_payment()
 		@payment.save
 		render :nothing => true, :status => :created
 	end
+
+	protected
+		def set_order
+			@user = User.find(params[:user_id].to_i)
+			@order = Order.where(id: params[:order_id].to_i).first
+		end
 end
