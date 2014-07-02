a428cad8728782b752febc18e8dec7fb346db061
kiwi <jcwen@thoughtworks.com>
2014-07-02 12:27:56 +0800

    simplify hash in payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,16 +3,16 @@ class PaymentsController < ApplicationController
 
 	def show
 		@payment = @order.payment
-		render :nothing => true, :status => :not_found unless @payment
+		render nothing: true, status: :not_found unless @payment
 	end
 
 	def create
 		if !@order.payment
 			@payment = @order.build_payment()
 			@payment.save
-			render :nothing => true, :status => :created, :location => user_order_payment_url(@user, @order)
+			render nothing: true, status: :created, location: user_order_payment_url(@user, @order)
 		else
-			render :nothing => true, :status => :bad_request
+			render nothing: true, status: :bad_request
 		end
 	end
 
